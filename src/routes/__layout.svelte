aa5f856e17a6034f06169aeb7e7b56298eed8e39
Diego Imbert <diegoimbert@pm.me>
2021-09-05 00:26:44 +0200

    First commit - The most beautiful front page I've ever done XD

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,24 @@
+<slot />
+<svelte:head>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap"
+    rel="stylesheet"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
+<style>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+  :global(body) {
+    @apply bg-black;
+    @apply text-white;
+    @apply overflow-x-hidden;
+    font-family: "Poppins";
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,50 @@
+<script>
+  import Typewriter from "svelte-typewriter";
+</script>
+
+<div class="flex justify-center">
+  <nav class="flex absolute items-center justify-center h-20 px-8 w-full max-w-3xl gap-8">
+    <p class="text-blue-300 opacity-60 hidden sm:block uppercase tracking-wide mr-auto">
+      <a href="/">Diego Imbert</a>
+    </p>
+    <p class="text-blue-300 opacity-60"><a href="/">Skills</a></p>
+    <p class="text-blue-300 opacity-60"><a href="/">Portfolio</a></p>
+    <p class="text-pink-500 relative z-10">
+      <strong><a href="/">Contact me</a></strong>
+    </p>
+  </nav>
+</div>
+
+<div class="pointer-events-none select-none">
+  <img
+    src="/night-sky.jpg"
+    alt=""
+    class="w-screen h-screen object-cover absolute"
+    style="z-index: -1;"
+  />
+
+  <div class="w-screen h-screen grid place-items-center absolute pb-12 overflow-hidden">
+    <Typewriter loop interval={64} delay={1024} cursor={false}>
+      {#each ["Software Engineer", "UI / UX Designer", "Software Developer", "Web Developer"] as title}
+        <h1
+          style="font-family: 'Oswald';"
+          class="text-5xl lg:text-8xl xl:text-9xl tracking-widest uppercase font-extralight text-center relative break-words"
+        >
+          {title}
+        </h1>
+      {/each}
+    </Typewriter>
+  </div>
+
+  <div class="w-screen h-screen flex justify-center overflow-hidden absolute">
+    <div
+      class="w-screen bg-black absolute bottom-0 blur-sm"
+      style="border-radius: 100% 100% 0 0; height: 32rem; transform: translateY(50%) scaleX(130%); min-width: 80rem;"
+    />
+    <img
+      src="/character/typing-1.png"
+      alt=""
+      class="absolute bottom-0 max-w-2xl 2xl:max-w-3xl max-h-full object-contain translate-y-16"
+    />
+  </div>
+</div>
