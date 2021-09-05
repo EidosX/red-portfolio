a186a4dda3b943bbda6872f73fe7087cf6e08cdc
Diego Imbert <diegoimbert@pm.me>
2021-09-05 15:32:44 +0200

    Fixed peaking shooting star on mobile

--- src/routes/components/ShootingStar.svelte
+++ src/routes/components/ShootingStar.svelte
@@ -13,14 +13,17 @@
 <style>
   @keyframes star-animation {
     0% {
+      opacity: 1;
       transform: var(--trans) translate(0, 0);
     }
     100% {
+      opacity: 1;
       transform: var(--trans) translate(100vw, 100vh);
     }
   }
 
   .star {
+    opacity: 0;
     width: 8rem;
     height: 0.15rem;
     --trans: rotate(20deg) skewX(88deg);
